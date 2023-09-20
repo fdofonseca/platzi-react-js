498fd0dd259427ddcd006fcb334a27c9ae7f3667
Fernando Alberto Fonseca Cofré <fafonseca@indracompany.com>
2023-09-20 15:22:03 -0300

    Initial Commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,7 @@
+function TodoCounter() {
+    return (
+        <h1>Has Completado 3 de 5 TODOs</h1>
+    );
+}
+
+export { TodoCounter }
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+    return (
+        <li>
+            <span>V</span>
+            <p>Tarea</p>
+            <span>X</span>
+        </li>
+    );
+}
+
+export { TodoItem }
