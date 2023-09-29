859039b77e2d0700cdfc525683e1b014a47e3e9c
Fernando Alberto Fonseca Cofré <fafonseca@indracompany.com>
2023-09-29 10:02:38 -0300

    localStorage

--- src/App.js
+++ src/App.js
@@ -6,16 +6,30 @@ import { CreateTodoButton } from './CreateTodoButton';
 import React from 'react';
 
 
-const defaultTodos = [
-  { text: 'Cortar Manzanas', completed: true },
-  { text: 'Cortar Platanos', completed: false },
-  { text: 'Cortar Naranjas', completed: true },
-  { text: 'Cortar Naranjas 2', completed: false },
-]
+//  const defaultTodos = [
+//    { text: 'Cortar Manzanas', completed: true },
+//    { text: 'Cortar Platanos', completed: false },
+//    { text: 'Cortar Naranjas', completed: true },
+//    { text: 'Cortar Naranjas 2', completed: false },
+//  ]
+//  localStorage.setItem('todos_list', JSON.stringify(defaultTodos));
 
 function App() {
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('todos_list');
+
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('todos_list', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  parsedTodos = JSON.parse(localStorageTodos);
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
   const completedTodos = todos.filter(todo => todo.completed).length;
   const totalTodos = todos.length;
@@ -25,18 +39,23 @@ function App() {
     return todoText.includes(searchText);
   });
 
+  const saveTodos = (newTodos) => {
+    localStorage.setItem('todos_list', JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(todo => todo.text === text);
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   const deleteTodo = (text) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex(todo => todo.text === text);
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   }
 
   return (
