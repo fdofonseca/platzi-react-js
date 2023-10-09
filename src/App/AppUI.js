21258e3c14db34afc0e4d43db0b24fea7f651252
Fernando Fonseca Cofré <fdo.fonseca97@gmail.com>
2023-10-09 19:03:51 -0300

    React Portal [Completo] + OpenModal + Add TODO

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -1,61 +1,60 @@
-import React from 'react';
-import { TodoCounter } from '../components/TodoCounter/index';
-import { TodoSearch } from '../components/TodoSearch/index';
-import { TodoList } from '../components/TodoList/index';
-import { TodoItem } from '../components/TodoItem/index';
-import { TodosLoading } from '../components/TodosLoading/index';
-import { TodosError } from '../components/TodosError/index';
-import { EmptyTodos } from '../components/EmptyTodos/index';
-import { CreateTodoButton } from '../components/CreateTodoButton/index';
-import { TodoContext } from '../TodoContext';
-import { Modal } from '../components/Modal';
+import React from "react";
+import { TodoCounter } from "../components/TodoCounter/index";
+import { TodoSearch } from "../components/TodoSearch/index";
+import { TodoList } from "../components/TodoList/index";
+import { TodoItem } from "../components/TodoItem/index";
+import { TodosLoading } from "../components/TodosLoading/index";
+import { TodosError } from "../components/TodosError/index";
+import { EmptyTodos } from "../components/EmptyTodos/index";
+import { CreateTodoButton } from "../components/CreateTodoButton/index";
+import { TodoContext } from "../TodoContext";
+import { Modal } from "../components/Modal";
+import { TodoForm } from "../components/TodoForm";
 
 function AppUI() {
+  const {
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    loading,
+    error,
+    openModal,
+    setOpenModal,
+  } = React.useContext(TodoContext);
 
-    const {
-        searchedTodos,
-        completeTodo,
-        deleteTodo,
-        loading,
-        error,
-        openModal,
-        setOpenModal
-    } = React.useContext(TodoContext);
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {loading && (
+          <>
+            <TodosLoading />
+            <TodosLoading />
+            <TodosLoading />
+          </>
+        )}
+        {error && <TodosError />}
+        {!loading && searchedTodos.lenght === 0 && <EmptyTodos />}
+        {searchedTodos.map((todo) => (
+          <TodoItem
+            text={todo.text}
+            completed={todo.completed}
+            key={todo.text}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
+          />
+        ))}
+      </TodoList>
+      <CreateTodoButton setOpenModal={setOpenModal} />
 
-    return (
-        <React.Fragment>
-            <TodoCounter />
-            <TodoSearch />
-            <TodoList>
-                {loading && <>
-                    <TodosLoading />
-                    <TodosLoading />
-                    <TodosLoading />
-                </>}
-                {error && <TodosError />}
-                {(!loading && searchedTodos.lenght === 0) && <EmptyTodos />}
-                {searchedTodos.map(todo => (
-                    <TodoItem
-                        text={todo.text}
-                        completed={todo.completed}
-                        key={todo.text}
-                        onComplete={() =>
-                            completeTodo(todo.text)}
-                        onDelete={() =>
-                            deleteTodo(todo.text)}
-                    />
-                ))}
-            </TodoList>
-            <CreateTodoButton />
-
-            {openModal && (
-                <Modal>
-                    Funcionalidad Agregar Todo
-                </Modal>
-            )}
-
-        </React.Fragment>
-    );
+      {openModal && (
+        <Modal>
+          <TodoForm />
+        </Modal>
+      )}
+    </React.Fragment>
+  );
 }
 
-export { AppUI }
+export { AppUI };
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './Modal.css';
 
 function Modal({ children }) {
     return ReactDOM.createPortal(
-        <div className="Modal">
-            {children}
-        </div>,
-        document.getElementById('modal')
+      <div className="ModalBackground">
+        {children}
+      </div>,
+      document.getElementById('modal')
     );
-}
-
-export { Modal };
+  }
+  
+  export { Modal };
